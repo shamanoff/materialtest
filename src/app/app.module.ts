e23e2c41d75ff27efc84f028b3305213cd359d9c
Shamanoff <shamanoff@gmail.com>
2017-09-11 16:57:27 +0300

    work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import {
+  MdButtonModule, MdCardModule, MdIconModule, MdMenuModule, MdSidenavModule,
+  MdToolbarModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
