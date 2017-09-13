7eddc3aca01ef4f149601157163f972ac2b735d5
Shamanoff <shamanoff@gmail.com>
2017-09-13 10:41:04 +0300

    work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import 'hammerjs';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
@@ -8,11 +8,20 @@ import {
   MdButtonModule, MdCardModule, MdIconModule, MdMenuModule, MdSidenavModule,
   MdToolbarModule
 } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { AsideComponent } from './aside/aside.component';
+import { BsideComponent } from './bside/bside.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AsideComponent,
+    BsideComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -21,7 +30,8 @@ import {
     MdMenuModule,
     MdToolbarModule,
     MdIconModule,
-    MdCardModule
+    MdCardModule,
+    FlexLayoutModule
 
   ],
   providers: [],
---
+++ src/app/bside/bside.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bside',
+  templateUrl: './bside.component.html',
+  styleUrls: ['./bside.component.scss']
+})
+export class BsideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
